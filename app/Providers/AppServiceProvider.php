274ba6945b62ae96b03869a2964135efb4b9ed2b
Oryuta13 <ryuta1113abcdol@gmail.com>
2024-03-07 00:04:36 +0900

    provider設定の更新

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,6 +38,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
