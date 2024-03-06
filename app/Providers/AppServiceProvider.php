7056af901f1bd4b35322d0b430ef7ec1556eba02
Oryuta13 <ryuta1113abcdol@gmail.com>
2024-03-07 00:25:42 +0900

    session-driverの設定をcookieに変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,6 +40,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // productionに設定
         if (App::environment(['production'])) {
             URL::forceScheme('https');
         }
