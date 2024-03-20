8531c339cf1c230abbccf021d8f8d4fab097b4a7
Oryuta13 <ryuta1113abcdol@gmail.com>
2024-03-21 01:31:44 +0900

    migrationファイルのavatarカラムを修正

--- database/migrations/2024_02_03_022901_add_avatar_and_introduction_to_users_table.php
+++ database/migrations/2024_02_03_022901_add_avatar_and_introduction_to_users_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             // 新しいカラムを追加する
-            // 新規登録時のavatarの初期値はs3のgray-icon.pngをUserモデルで指定している為、コメントアウト
-            // $table->string('avatar')->nullable()->default('/storage/avatars/gray-icon.png');
+            // 新規登録時のavatarの初期値はs3のgray-icon.pngをUserモデルで指定している為、初期値は空
+            $table->string('avatar')->nullable();
             $table->string('introduction')->nullable()->default('自己紹介文を入力する自己紹介文を入力する自己紹介文を入力する自己紹介文を入力する自己紹介文を入力する自己紹介文を入力する');
         });
     }
